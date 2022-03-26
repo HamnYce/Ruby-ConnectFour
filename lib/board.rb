b9bbc1d44bb427526ef3e99bdc1b9d0afd4cb9b9
HamnYce <hamadboa@gmail.com>
2022-03-26 15:33:41 +0300

    add #place_checker method

--- lib/board.rb
+++ lib/board.rb
@@ -2,13 +2,35 @@
 
 # Contains state of board
 class Board
+  attr_reader :board, :pos
+
   def initialize
-    @state = Array.new(8, Array.new(8, nil))
+    @board = Array.new(8) { Array.new(8, ' ') }
   end
 
-  def place_checker(column)
-    # player chooses column in other script method,
-    # keeps going down in  rows (i) until next board(i)(column) is != nil |
-    # out_of_bounds
+  # after player chooses column, we loop starting from the last row
+  # once nil is found then places the checker
+  # returns position of placed checker
+  def place_checker(col, checker)
+    return @pos = [-1, -1] unless col.between?(0, 7)
+
+    7.downto(0) do |row|
+      if @board[row][col] == ' '
+        @board[row][col] = checker
+        return @pos = [row, col]
+      end
+    end
+
+    @pos = [-1, -1]
+  end
+
+  def to_s
+    s = ''
+
+    @board.each do |row|
+      s += "#{row.join('|')}\n"
+    end
+
+    s
   end
 end
