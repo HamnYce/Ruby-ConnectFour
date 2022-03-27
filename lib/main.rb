7af9fcf6a5131b68c626dcff4ba8e513cbd83d02
HamnYce <hamadboa@gmail.com>
2022-03-27 11:14:40 +0300

    only create new driver

--- lib/main.rb
+++ lib/main.rb
@@ -1,16 +1,10 @@
 # frozen_string_literal: true
 
-# Board Class
-# Driver Class
-# Winner Module
-# Text Module
-
 # \u26AA (white checker)
 # \u26AB (black checker)
 
 # 8 columns by 8 rows
 
-winner_board = Array.new(8) { Array.new(8, nil) }
-(0..3).each { |i| winner_board[7][i] = 'x' }
+require_relative 'driver'
 
-winner_board.each { |arr| p arr }
+Driver.new
